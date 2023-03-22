a9380263246dbdebe51afbcf8d88ec78b0b4ae6a
Oriol-Vilalta <120737270+Oriol-Vilalta@users.noreply.github.com>
2023-03-22 18:51:55 +0100

    Add files via upload

--- graph-designer.py
+++ graph-designer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+import subprocess
 import sys
 import networkx
 
 colors = ['#0000FF', '#00FF00', '#FF0000', '#FFFF00', '#FF00FF', '#00FFFF', '#FFFFFF']
+default_output_name = "out.png"
 values = []
 
-num_nodes = int(sys.argv[2])
-num_colors = int(sys.argv[3])
+num_nodes = 0
+num_colors = 0
 
 
 def get_node_possibilities(node):
@@ -24,9 +26,41 @@ def paint_nodes(A):
         A.get_node(i).attr['fillcolor'] = get_color_hex_from_sublist(get_node_possibilities(i))
 
 
-def draw_graph(A):
+def save_graph(A):
     A.layout()
-    A.draw("out.png", format='png')
+    if len(sys.argv) > 2:
+        output_name = sys.argv[2]
+    else:
+        output_name = default_output_name
+    A.draw(output_name, format='png')
+
+
+def calculate_skipped_lines():
+    tmp = 0
+    for i in range(1, num_colors):
+        tmp += i
+    return 2 + tmp * num_nodes + num_nodes
+
+
+def create_edges(A):
+    try:
+        cnf_file = open(sys.argv[1], "r")
+        lines = cnf_file.readlines()
+        cnf_file.close()
+    except IOError:
+        pass
+    lines = lines[calculate_skipped_lines():]
+    print(calculate_skipped_lines())
+    num_edges = int(len(lines) / num_colors)
+    for i in range(num_edges):
+        edge = lines[i * num_colors]
+        edge = edge.split()[:-1]
+        for j in range(2):
+            edge[j] = int(edge[j])
+            edge[j] = abs(edge[j])
+            edge[j] = int((edge[j] - 1) / num_colors)
+        print(edge)
+        A.add_edge(tuple(edge))
 
 
 def print_graph(graph):
@@ -36,7 +70,8 @@ def print_graph(graph):
     A.node_attr['height'] = '0.4'
     A.edge_attr['color'] = '#000000'
     paint_nodes(A)
-    draw_graph(A)
+    create_edges(A)
+    save_graph(A)
 
 
 def node_generation():
@@ -47,20 +82,21 @@ def node_generation():
 
 
 def read_file():
-    global values
-    try:
-        with open(f"{sys.argv[1]}", "r") as file:
-            for line in file:
-                if line.startswith("v"):
-                    values = line[1:].split()
-                    for i in range(len(values)):
-                        values[i] = int(values[i])
-                    break
-        file.close()
-    except IOError:
-        exit(1)
+    global values, num_nodes
+    result = subprocess.run(["python3", "solver-pvp.py", f"{sys.argv[1]}"], capture_output=True, text=True)
+    solver_output = result.stdout.split("\n")
+
+    if solver_output[0] == "s SATISFIABLE":
+        print("Satisfible solution")
+        if solver_output[1].startswith("v"):
+            values = solver_output[1].split()[1:]
+            for i in range(len(values)):
+                values[i] = int(values[i])
+                if values[i] > 0:
+                    num_nodes += 1
 
 
 if __name__ == '__main__':
     read_file()
+    num_colors = int(len(values) / num_nodes)
     print_graph(node_generation())
